c495b5d6383bd266d0c541101691667a0c4e6736
Jan-Paul Kleemans <jpkleemans@gmail.com>
2017-05-10 16:52:54 +0200

    Accept options

--- store.js
+++ store.js
@@ -1,33 +1,32 @@
 const R = require('ramda')
 const datastore = require('@google-cloud/datastore')
 
-module.exports = (projectId, namespace) => {
-  const store = datastore({projectId})
+module.exports = (options) => {
+  const store = datastore(options)
 
   const generateKey = (kind) => store.key({
-    namespace,
     path: [kind]
   })
 
-  return {
-    /**
-     * Save data into the store.
-     *
-     * @param {string} kind
-     * @param {Object} data
-     * @return {Promise}
-     */
-    save: (kind, data) => store.save({
-      key: generateKey(kind),
-      data
-    }),
+  /**
+   * Save data into the store.
+   *
+   * @param {string} kind
+   * @param {Object} data
+   * @return {Promise}
+   */
+  const save = (kind, data) => store.save({
+    key: generateKey(kind),
+    data
+  })
 
-    /**
-     * Find all data of a given kind.
-     *
-     * @param {string} kind
-     * @return {Promise}
-     */
-    findAll: R.pipe(store.createQuery, store.runQuery)
-  }
+  /**
+   * Find all data of a given kind.
+   *
+   * @param {string} kind
+   * @return {Promise}
+   */
+  const findAll = R.pipe(store.createQuery, store.runQuery)
+
+  return {save, findAll}
 }
